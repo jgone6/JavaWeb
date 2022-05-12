45b9b04b3f639c3d699b84de365943be9668b016
jgone6 <62082028+jgone6@users.noreply.github.com>
2022-05-12 15:56:54 +0900

    20220512
    register

---
+++ BackEnd/web_project/src/main/java/team_6/web_project/WebProjectApplication.java
@@ -0,0 +1,14 @@
+package team_6.web_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class WebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebProjectApplication.class, args);
+    }
+
+}
